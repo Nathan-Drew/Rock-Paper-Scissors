22c0ec9eef8acb1b0ee1663d2dbe989d0d0c94ed
NathanDrew <nathandrew118@gmail.com>
2017-02-11 14:51:31 +0000

    Rock, Paper, Scissors
    Designed a version of the classic ‘Rock, Paper, Scissors’ game in order
    to practice Javascript. Only vanilla JS was utilised (i.e. no
    frameworks).
    
    I haven’t paid much attention to the design (beyond utilising Bootstrap
    to ensure responsiveness) as this was more about getting the program to
    run.

---
+++ js/index.js
@@ -0,0 +1,57 @@
+var Rock = document.getElementById('Rock').addEventListener('click', run);
+var Paper = document.getElementById('Paper').addEventListener('click', run);
+var Scissors = document.getElementById('Scissors').addEventListener('click', run);
+
+var computerChoice;
+var playerChoice;
+
+var player = document.getElementById('player');
+var computer = document.getElementById('computer');
+var outcome = document.getElementById('outcome');
+
+function run() {
+  	playerChoice = this.innerText;
+  	computerGamble();
+  	compare();
+}
+
+function computerGamble() {
+  	var dice = Math.random();
+	if (dice <= 0.33) {
+		computerChoice = "Rock";
+    } else if (dice > 0.33 && dice <= 0.66) {
+    	computerChoice = "Paper";
+    } else {
+    	computerChoice = "Scissors";
+    }
+ }
+ 
+ function compare() {
+ 	player.innerHTML = "You have chosen " + playerChoice;
+ 	computer.innerHTML = "Computer chooses " + computerChoice;
+
+ 	if (playerChoice == computerChoice) {
+ 		outcome.innerHTML = "It's a tie!";
+ 		
+ 	} else if (playerChoice === "Rock" && computerChoice === "Paper") {
+ 		outcome.innerHTML = "The computer wins!";
+ 	} else if (playerChoice === "Rock" && computerChoice === "Scissors") {
+ 		outcome.innerHTML = "You win!";
+ 	
+
+ 	} else if (playerChoice === "Paper" && computerChoice === "Rock") {
+ 		outcome.innerHTML = "You win!";
+ 	} else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+ 		outcome.innerHTML = "The computer wins!";
+
+
+ 	} else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+ 		outcome.innerHTML = "The computer wins!";
+ 	} else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+ 		outcome.innerHTML = "You win!";
+ 	}
+ }
+
+
+
+
